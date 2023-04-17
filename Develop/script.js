d90cefae9d7eb0a261a3be68f245333de70a3af3
Robyn Smith <robynsmith21@hotmail.com>
2023-04-17 21:23:09 +0100

    first draft javascript and read me

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,24 @@ function writePassword() {
   passwordText.value = password;
 
 }
+//find users preferances
+function generatePassword(){
+  const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const lowercase = "abcdefghijklmnopqrstuvwxyz"
+  const numbers = "0123456789"
+  const specialCharacters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
+  var uppercase = false;
+  var lowercase = false;
+  var numbers = false;
+  var special = false;
+  var length = 0
+
+
+
+
+  
+  return ('')
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
